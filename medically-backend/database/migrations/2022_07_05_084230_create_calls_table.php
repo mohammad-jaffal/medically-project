aa309f65b5f0339b2806ee2fff8fa57b69b3059f
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-05 11:48:09 +0300

    Migrating database tables

--- medically-backend/database/migrations/2022_07_05_084230_create_calls_table.php
+++ medically-backend/database/migrations/2022_07_05_084230_create_calls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddToUsersTable extends Migration
+class CreateCallsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,12 @@ class AddToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->integer('type');
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('doctor_id');
+            $table->integer('duration');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +29,6 @@ class AddToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('calls');
     }
 }
