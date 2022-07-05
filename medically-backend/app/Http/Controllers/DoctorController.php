42c93905b2ca04d48d035dd2515565d79ab7f3c1
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-05 15:44:46 +0300

    Creating add doctor api

--- medically-backend/app/Http/Controllers/DoctorController.php
+++ medically-backend/app/Http/Controllers/DoctorController.php
@@ -9,5 +9,21 @@ use Illuminate\Http\Request;
 class DoctorController extends Controller
 {
  
-    
+    public function addDoctor(Request $request){
+
+        $doctor = new Doctor_detail;
+
+        $doctor->doctor_id = $request->doctor_id;
+        $doctor->rating = 0;
+        $doctor->channel_name = $request->channel_name;
+        $doctor->channel_token = $request->channel_token;
+        $doctor->bio = 'empty doctor bio';
+        $doctor->category_id = $request->category_id;
+
+        $doctor->save();
+
+        return response()->json([
+            "success" => true,
+        ], 200);
+    }
 }
