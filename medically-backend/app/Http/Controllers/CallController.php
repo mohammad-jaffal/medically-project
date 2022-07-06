7f150ca378ea1bb851e3e882c47d7ae8240b2e87
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-06 09:53:11 +0300

    Creating add call api

--- medically-backend/app/Http/Controllers/CallController.php
+++ medically-backend/app/Http/Controllers/CallController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Call;
 
 use Illuminate\Http\Request;
 
 class CallController extends Controller
 {
-    //
+    public function addCall(Request $request){
+        
+        $call = new Call;
+        $call->user_id = $request->user_id;
+        $call->doctor_id = $request->doctor_id;
+        $call->duration = $request->duration;
+        $call->save();
+
+        return response()->json([
+            "success" => true,
+        ], 200);
+    }
 }
--- medically-backend/routes/api.php
+++ medically-backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\JWTController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\DomainController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CallController;
 
 Route::group(['middleware' => 'api'], function($router) {
     Route::post('/register', [JWTController::class, 'register']);
@@ -25,6 +26,8 @@ Route::group(['prefix'=>'admin'], function(){
 Route::group(['prefix'=>'user'], function(){
     Route::post('/add-favorite', [FavoriteController::class, 'addFavorite']);
     Route::post('/get-favorites', [FavoriteController::class, 'getFavoritesByUserID']);
+    Route::post('/add-call', [CallController::class, 'addCall']);
+
 });
 
 
