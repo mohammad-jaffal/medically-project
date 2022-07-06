0412bf0d918e641f5d975018bc4e0aa78f04870f
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-06 10:13:25 +0300

    Renaming get-calls api to get-user-calls

--- medically-backend/routes/api.php
+++ medically-backend/routes/api.php
@@ -29,7 +29,7 @@ Route::group(['prefix'=>'user'], function(){
     Route::post('/get-favorites', [FavoriteController::class, 'getFavoritesByUserID']);
 
     Route::post('/add-call', [CallController::class, 'addCall']);
-    Route::post('/get-calls', [CallController::class, 'getCallsByUserID']);
+    Route::post('/get-user-calls', [CallController::class, 'getCallsByUserID']);
 
     Route::post('/add-review', [ReviewController::class, 'addReview']);
 
