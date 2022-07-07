577ce4ef461bbebc31e601a0a1426687509063fb
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 12:40:38 +0300

    Frontend: Creating empty forms for login and signup

--- medically_frontend/lib/screens/login_screen.dart
+++ medically_frontend/lib/screens/login_screen.dart
@@ -10,10 +10,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _loginFormKey = GlobalKey<FormState>();
+  final _signupFormKey = GlobalKey<FormState>();
+  final _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('login screen here ')),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: _isLogin
+              ? Form(
+                  key: _loginFormKey,
+                  child: Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 0, 4, 255),
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Form(
+                  key: _signupFormKey,
+                  child: Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Text(
+                        'SignUp',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 0, 4, 255),
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        ),
+      ),
     );
   }
 }
