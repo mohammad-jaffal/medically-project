21ce34f609e135250f5be17dc8639011167b967d
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 14:15:40 +0300

    Frontend: Initialising bottom bar screen

--- medically_frontend/lib/screens/bottom_bar.dart
+++ medically_frontend/lib/screens/bottom_bar.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class BottomAppBar extends StatefulWidget {
-  const BottomAppBar({Key? key}) : super(key: key);
+class BottomBarScreen extends StatefulWidget {
+  const BottomBarScreen({Key? key}) : super(key: key);
 
   @override
-  State<BottomAppBar> createState() => _BottomAppBarState();
+  State<BottomBarScreen> createState() => _BottomBarScreenState();
 }
 
-class _BottomAppBarState extends State<BottomAppBar> {
+class _BottomBarScreenState extends State<BottomBarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
