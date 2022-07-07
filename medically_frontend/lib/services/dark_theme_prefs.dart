d3fbef83006e37a0bd6ec9ac726de0cb865c7dca
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 10:16:08 +0300

    Frontend: Creating gettheme methode

--- medically_frontend/lib/services/dark_theme_prefs.dart
+++ medically_frontend/lib/services/dark_theme_prefs.dart
@@ -7,4 +7,9 @@ class DarkThemePrefs {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool(THEME_STATUS, value);
   }
+
+  Future<bool> getTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(THEME_STATUS) ?? false;
+  }
 }
