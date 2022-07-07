b88bcbc82384e9620530349d5f3577b1a23f820d
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 10:24:04 +0300

    Frontend: Creating setdarktheme methode in darkthemeprovider

--- medically_frontend/lib/providers/dark_theme_provider.dart
+++ medically_frontend/lib/providers/dark_theme_provider.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:medically_frontend/services/dark_theme_prefs.dart';
 
 class DarkThemeProvider with ChangeNotifier {
+  DarkThemePrefs darkThemePrefs = DarkThemePrefs();
+
   bool _darkTheme = false;
 
   bool get getDarkTheme => _darkTheme;
+
+  set setDarkTheme(bool value) {
+    _darkTheme = value;
+    darkThemePrefs.setDarkTheme(value);
+    notifyListeners();
+  }
 }
