9c0bd591b68b769d70e6838207bd9e9e6acd2127
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 15:08:11 +0300

    Frontend: Changing bottom navigation bar style

--- medically_frontend/lib/consts/theme_data.dart
+++ medically_frontend/lib/consts/theme_data.dart
@@ -7,25 +7,26 @@ class Styles {
           isDarkTheme ? const Color(0xFF00001a) : const Color(0xFFFFFFFF),
       primaryColor: isDarkTheme
           ? const Color(0xFF00001a)
-          : Color.fromARGB(255, 0, 4, 255),
+          : const Color.fromARGB(255, 0, 4, 255),
       // Color.fromARGB(255, 0, 4, 255),
       colorScheme: ThemeData().colorScheme.copyWith(
             secondary:
                 isDarkTheme ? const Color(0xff1a1f3c) : const Color(0xffe8fdfd),
             brightness: isDarkTheme ? Brightness.dark : Brightness.light,
           ),
-      cardColor:
-          isDarkTheme ? const Color(0xff0a0d2c) : const Color(0xfff2fdfd),
-      canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
-      buttonTheme: Theme.of(context).buttonTheme.copyWith(
-          colorScheme: isDarkTheme
-              ? const ColorScheme.dark()
-              : const ColorScheme.light()),
 
+      // cardColor:
+      //     isDarkTheme ? const Color(0xff0a0d2c) : const Color(0xfff2fdfd),
+      canvasColor:
+          isDarkTheme ? Colors.black : const Color.fromARGB(255, 0, 4, 255),
+      // buttonTheme: Theme.of(context).buttonTheme.copyWith(
+      //     colorScheme: isDarkTheme
+      //         ? const ColorScheme.dark()
+      //         : const ColorScheme.light()),
       // app bar theme
       appBarTheme: AppBarTheme(
         backgroundColor:
-            isDarkTheme ? Colors.black : Color.fromARGB(255, 0, 4, 255),
+            isDarkTheme ? Colors.black : const Color.fromARGB(255, 0, 4, 255),
         // This will be applied to the "back" icon
         // iconTheme: IconThemeData(color: Colors.white),
         // This will be applied to the action icon buttons that locates on the right side
--- medically_frontend/lib/screens/bottom_bar.dart
+++ medically_frontend/lib/screens/bottom_bar.dart
@@ -39,36 +39,27 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
     return Scaffold(
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomAppBar(
-        child: Container(
-          decoration: const BoxDecoration(
-            border: Border(
-              top: BorderSide(width: 0.5),
+        child: BottomNavigationBar(
+          onTap: _selectScreen,
+          currentIndex: _selectedIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
             ),
-          ),
-          child: BottomNavigationBar(
-            onTap: _selectScreen,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-            currentIndex: _selectedIndex,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.list),
-                label: "MyList",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.call),
-                label: "Logs",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: "Me",
-              ),
-            ],
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: "MyList",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: "Logs",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Me",
+            ),
+          ],
         ),
       ),
     );
