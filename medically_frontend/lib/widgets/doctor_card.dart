1b1c8cc8368f00b9d984c578fd2fd7cdfdd9adb7
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-07 17:38:04 +0300

    Frontend: Showing profile picture in doctor card

--- medically_frontend/lib/widgets/doctor_card.dart
+++ medically_frontend/lib/widgets/doctor_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:medically_frontend/models/doctor.dart';
+import 'dart:convert';
 
 class DoctorCard extends StatelessWidget {
   final Doctor doctor;
@@ -12,6 +13,8 @@ class DoctorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var _bytesImage = Base64Decoder().convert(doctor.base64Image);
+
     return InkWell(
       child: Card(
         child: Container(
@@ -19,7 +22,23 @@ class DoctorCard extends StatelessWidget {
             // color: Colors.lightBlue,
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Text(doctor.name),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.memory(
+                    _bytesImage,
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // child: Text(doctor.name),
         ),
       ),
     );
