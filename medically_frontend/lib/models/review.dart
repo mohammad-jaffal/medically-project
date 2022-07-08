d91b7635757a81bc98db4d758b1a77267cee305a
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-08 22:06:21 +0300

    Frontend: Creating review model

---
+++ medically_frontend/lib/models/review.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+class Review {
+  final int id;
+  final int user_id;
+  final int doctor_id;
+  final int rating;
+  final int review_text;
+
+  Review({
+    required this.id,
+    required this.user_id,
+    required this.doctor_id,
+    required this.rating,
+    required this.review_text,
+  });
+
+  factory Review.fromJson(Map<String, dynamic> json) {
+    return Review(
+      id: json['id'],
+      user_id: json['user_id'],
+      doctor_id: json['doctor_id'],
+      rating: json['rating'],
+      review_text: json['review_text'],
+    );
+  }
+}
