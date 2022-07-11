cb1a6858b2c8a673aca621f4e91a2331e225dc58
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-11 15:44:30 +0300

    Frontend: Implementing balance screen

--- medically_frontend/lib/screens/balance_screen.dart
+++ medically_frontend/lib/screens/balance_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:medically_frontend/widgets/bundle_card.dart';
 
 class BalanceScreen extends StatelessWidget {
   const BalanceScreen({Key? key}) : super(key: key);
@@ -9,12 +10,49 @@ class BalanceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map> bundles = [
+      {
+        'amount': 50,
+        'price': 5,
+      },
+      {
+        'amount': 100,
+        'price': 10,
+      },
+      {
+        'amount': 150,
+        'price': 15,
+      },
+      {
+        'amount': 200,
+        'price': 20,
+      },
+      {
+        'amount': 500,
+        'price': 50,
+      },
+      {
+        'amount': 1000,
+        'price': 100,
+      },
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Select bundle'),
       ),
-      body: Center(
-        child: Text('Balance screen'),
+      body: GridView.builder(
+        // controller: _controller,
+        padding: const EdgeInsets.all(10),
+        itemCount: bundles.length,
+        itemBuilder: (ctx, i) => BundleCard(
+          amount: bundles[i]['amount'],
+          price: bundles[i]['price'],
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          childAspectRatio: 5 / 1,
+          // mainAxisSpacing: 30,
+        ),
       ),
     );
   }
--- medically_frontend/lib/widgets/bundle_card.dart
+++ medically_frontend/lib/widgets/bundle_card.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/dark_theme_provider.dart';
 
 class BundleCard extends StatelessWidget {
-  const BundleCard({Key? key}) : super(key: key);
-
+  const BundleCard({
+    Key? key,
+    required this.amount,
+    required this.price,
+  }) : super(key: key);
+  final int amount;
+  final int price;
   @override
   Widget build(BuildContext context) {
-    return const InkWell(
-      child: GridTile(
-        child: Icon(Icons.monetization_on_rounded),
-        footer: GridTileBar(title: Text('100')),
+    final themeState = Provider.of<DarkThemeProvider>(context);
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '$amount coins',
+              style: const TextStyle(fontSize: 20),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  themeState.getDarkTheme
+                      ? const Color(0xFF00001a)
+                      : const Color.fromARGB(255, 54, 135, 255),
+                ),
+              ),
+              child: Text('$price \$'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+// themeState.getDarkTheme
+//                                   ? Colors.white
+//                                   : Colors.grey[600],
