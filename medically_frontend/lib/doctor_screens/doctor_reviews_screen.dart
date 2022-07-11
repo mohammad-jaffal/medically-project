46b01381aec33982e55f24b8e3c47cafc6b541ed
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-11 21:07:54 +0300

    Frontend: Implementing doctor's reviews screen

--- medically_frontend/lib/doctor_screens/doctor_reviews_screen.dart
+++ medically_frontend/lib/doctor_screens/doctor_reviews_screen.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/doctors_provider.dart';
+import '../providers/reviews_provider.dart';
+import '../widgets/review_card.dart';
 
 class DoctorReviewsScreen extends StatefulWidget {
   const DoctorReviewsScreen({Key? key}) : super(key: key);
@@ -12,8 +20,21 @@ class DoctorReviewsScreen extends StatefulWidget {
 class _DoctorReviewsScreenState extends State<DoctorReviewsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('doc reviews'),
+    final docId = 1;
+    // get reviews
+    final reviewsProvider = Provider.of<ReviewsProvider>(context);
+    List reviews = reviewsProvider.getReviewsByDocID(docId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reviews'),
+      ),
+      body: ListView.builder(
+        itemCount: reviews.length,
+        itemBuilder: (context, index) => ReviewCard(
+          reviews[index].rating * 1.0,
+          reviews[index].review_text,
+        ),
+      ),
     );
   }
 }
