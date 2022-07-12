199f3290cc292457276964a7ff7b451ef11ce6d7
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-12 12:18:50 +0300

    Frontend: Creating validate token methode in token provider

--- medically_frontend/lib/providers/token_provider.dart
+++ medically_frontend/lib/providers/token_provider.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class TokenProvider with ChangeNotifier {
   var _token;
 
   void set setToken(var token) {
     _token = token;
+  }
+
+  Future<bool> validateToken() async {
+    var url = Uri.parse('http://10.0.2.2:8000/api/profile');
+    var response = await http.post(url, headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $_token'
+    });
+    print('Response body: ${response.body}');
+    return false;
   }
 }
