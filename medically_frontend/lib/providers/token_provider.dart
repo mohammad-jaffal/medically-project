5988c9236d8eec2c1b5557b9e359c7d18943a06a
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-12 12:45:04 +0300

    Frontend: Setting the user in user provider with the fetched user

--- medically_frontend/lib/providers/token_provider.dart
+++ medically_frontend/lib/providers/token_provider.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:medically_frontend/models/user.dart';
+import 'package:medically_frontend/providers/user_provider.dart';
 
 class TokenProvider with ChangeNotifier {
   var _token;
@@ -9,14 +13,26 @@ class TokenProvider with ChangeNotifier {
     validateToken();
   }
 
-  Future<bool> validateToken() async {
-    print(_token);
+  Future<String> validateToken() async {
     var url = Uri.parse('http://10.0.2.2:8000/api/profile');
     var response = await http.post(url, headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer $_token'
     });
-    print('Response body: ${response.body}');
-    return false;
+    if (json.decode(response.body)['message'] == 'Unauthenticated.') {
+      return 'Unauthenticated';
+    }
+    print('Response body: ${json.decode(response.body)['id']}');
+    var u = User(
+      id: json.decode(response.body)['id'],
+      name: json.decode(response.body)['name'],
+      email: json.decode(response.body)['email'],
+      base64Image: json.decode(response.body)['profile_picture'],
+      balance: json.decode(response.body)['balance'],
+      type: json.decode(response.body)['type'],
+    );
+
+    UserProvider().setuser(u);
+    return 'false';
   }
 }
