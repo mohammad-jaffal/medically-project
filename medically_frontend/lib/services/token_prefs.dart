92b7bc8c7472acfde8651e76bd04cb6bca990b17
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-13 18:55:48 +0300

    Frontend: Creating set token in tokenprefs

--- medically_frontend/lib/services/token_prefs.dart
+++ medically_frontend/lib/services/token_prefs.dart
@@ -2,4 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenPrefs {
   static const MEDICALLY_TOKEN = "MEDICALLY_TOKEN";
+  setToken(String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(MEDICALLY_TOKEN, value);
+  }
 }
