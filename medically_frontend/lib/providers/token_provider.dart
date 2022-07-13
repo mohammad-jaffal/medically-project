fddec39931377ddfaa76ac097b020e40a7ee501e
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-13 19:01:04 +0300

    Frontend: Saving the token on login

--- medically_frontend/lib/providers/token_provider.dart
+++ medically_frontend/lib/providers/token_provider.dart
@@ -4,12 +4,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:medically_frontend/models/user.dart';
 import 'package:medically_frontend/providers/user_provider.dart';
+import 'package:medically_frontend/services/token_prefs.dart';
 
 class TokenProvider with ChangeNotifier {
+  TokenPrefs tokenPrefs = TokenPrefs();
   var _token;
 
   void setToken(var token) {
     _token = token;
+    tokenPrefs.setToken(token);
     // validateToken();
   }
 
