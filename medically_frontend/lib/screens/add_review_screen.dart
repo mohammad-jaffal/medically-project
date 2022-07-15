1b078ee09ce66c1711c72385d13fd7e34118c4f9
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-15 16:02:02 +0300

    Frontend: Creating send review button

--- medically_frontend/lib/screens/add_review_screen.dart
+++ medically_frontend/lib/screens/add_review_screen.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/dark_theme_provider.dart';
 
 class AddReviewScreen extends StatefulWidget {
-  const AddReviewScreen({Key? key}) : super(key: key);
+  AddReviewScreen({Key? key}) : super(key: key);
   static const routeName = '/add-review-screen';
 
   @override
@@ -13,9 +16,27 @@ class AddReviewScreen extends StatefulWidget {
 
 class _AddReviewScreenState extends State<AddReviewScreen> {
   var _rating = 0;
+  final myController = TextEditingController();
+
+  void _sendReview() {
+    if (_rating == 0 || myController.text == '') {
+      print('do nothing');
+    } else {
+      print(_rating);
+      print(myController.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("review"),
@@ -39,23 +60,47 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
               itemSize: 70,
               direction: Axis.horizontal,
             ),
-            const Card(
+            Card(
               child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
                 child: TextField(
                   // keyboardType: TextInputType.multiline,
                   textInputAction: TextInputAction.newline,
                   minLines: 1,
                   maxLines: 6,
-
-                  style: TextStyle(
+                  controller: myController,
+                  style: const TextStyle(
                     fontSize: 20,
                     wordSpacing: 1,
                     height: 1.5,
                   ),
+                  decoration: const InputDecoration(
+                    hintText: 'Write the review here...',
+                  ),
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  FloatingActionButton(
+                    onPressed: () {
+                      _sendReview();
+                    },
+                    backgroundColor: themeState.getDarkTheme
+                        ? const Color(0xff0a0d2c)
+                        : const Color.fromARGB(255, 54, 135, 255),
+                    child: const Icon(
+                      Icons.send,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
