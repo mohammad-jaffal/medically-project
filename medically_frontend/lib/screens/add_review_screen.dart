674a5b9304e38643df016fb2684ee3244afdb64f
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-15 15:38:49 +0300

    Frontend: Creating doctor review form

--- medically_frontend/lib/screens/add_review_screen.dart
+++ medically_frontend/lib/screens/add_review_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class AddReviewScreen extends StatelessWidget {
+class AddReviewScreen extends StatefulWidget {
   const AddReviewScreen({Key? key}) : super(key: key);
   static const routeName = '/add-review-screen';
+
+  @override
+  State<AddReviewScreen> createState() => _AddReviewScreenState();
+}
+
+class _AddReviewScreenState extends State<AddReviewScreen> {
+  var _rating = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +20,44 @@ class AddReviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("review"),
       ),
-      body: Center(
-        child: Text('review screen'),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            RatingBarIndicator(
+              rating: double.parse('$_rating'),
+              itemBuilder: (context, index) => IconButton(
+                onPressed: () {
+                  setState(() {
+                    _rating = index + 1;
+                  });
+                },
+                icon: const Icon(Icons.star),
+                color: Colors.amber,
+              ),
+              itemCount: 5,
+              itemSize: 70,
+              direction: Axis.horizontal,
+            ),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: TextField(
+                  // keyboardType: TextInputType.multiline,
+                  textInputAction: TextInputAction.newline,
+                  minLines: 1,
+                  maxLines: 6,
+
+                  style: TextStyle(
+                    fontSize: 20,
+                    wordSpacing: 1,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
