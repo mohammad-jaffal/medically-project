b13669667415ef0dfa362edd67981c2a93dda285
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-15 23:59:06 +0300

    Frontend: Fetching channel token in call screen

--- medically_frontend/lib/screens/user_call_screen.dart
+++ medically_frontend/lib/screens/user_call_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/doctors_provider.dart';
 
 class UserCallScreen extends StatelessWidget {
   const UserCallScreen({Key? key}) : super(key: key);
@@ -10,12 +13,15 @@ class UserCallScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final doctorId =
         int.parse(ModalRoute.of(context)!.settings.arguments.toString());
+    final doctorsProvider = Provider.of<DoctorsProvider>(context);
+    var doctor = doctorsProvider.getDoctorByID(doctorId);
+    print(doctor.channelToken);
     return Scaffold(
       appBar: AppBar(
         title: Text(doctorId.toString()),
       ),
       body: Center(
-        child: Text('User call screen'),
+        child: Text(doctor.channelName),
       ),
     );
   }
