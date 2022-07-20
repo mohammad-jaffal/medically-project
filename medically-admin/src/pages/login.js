5f9451f859952b498b8063de2352a2f420e02192
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-20 16:56:00 +0300

    Frontend: Creatingg admin login form

--- medically-admin/src/pages/login.js
+++ medically-admin/src/pages/login.js
@@ -1,6 +1,15 @@
 const Login = () =>{
     return(
-        <div>login page</div>
+        <div className="global-container">
+                <div className="login-form-container">
+                        <p>Login</p>
+                        <input type="text" id="li_email" className="login-input" placeholder="Email" required />
+                    
+                        <input type="password" id="li_password" className="login-input" placeholder="Password" required />
+                    
+                        <button className='li-btn' onClick={() => {}}>Login</button>
+                </div>
+            </div>
     );
 }
 
