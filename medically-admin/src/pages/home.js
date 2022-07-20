f3f94fa233250b3d6e6d32d545132f31426ea17b
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-20 22:26:24 +0300

    Frontend: Creating home page structure

--- medically-admin/src/pages/home.js
+++ medically-admin/src/pages/home.js
@@ -3,8 +3,15 @@ import Navbar from "../components/NavBar";
 const Home = () => {
     return (
         <div className="global-container">
-            <Navbar/>
-            <p>Admin Home</p>
+            <Navbar />
+            <div className="home-body">
+                <div className="filter-container">
+                    <p>Filter</p>
+                </div>
+                <div className="data-container">
+                    <p>Data</p>
+                </div>
+            </div>
         </div>
     );
 }
