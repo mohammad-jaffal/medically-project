ce34337499367ebe65b17a99e00537e409d3f12c
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-20 21:50:10 +0300

    Frontend: Creating and styling add domian ui

--- medically-admin/src/pages/AddDomain.js
+++ medically-admin/src/pages/AddDomain.js
@@ -3,9 +3,14 @@ import Navbar from "../components/NavBar";
 const AddDomain = () => {
     return (
         <div className="global-container">
-            <Navbar/>
+            <Navbar />
             <div className="add-domain-body">
-                <p>add domain</p>
+                <p>Add Domain:</p>
+                <div className="domain-input-container">
+                    <input type="text" className="domain-input" required />
+                    <button className="admin-btn">Add</button>
+                </div>
+                
             </div>
         </div>
     );
