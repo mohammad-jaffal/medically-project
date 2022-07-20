8a3472a28f337b0ba9a1711b56f1cd2517ad4be6
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-20 17:26:04 +0300

    Frontend: Adding admin home page to routes table

--- medically-admin/src/App.js
+++ medically-admin/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/login';
+import Home from './pages/home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Login />}/>
+        <Route path={"/home"} element={<Home />}/>
       </Routes>
     </BrowserRouter>
   );
