103b13e31277532be6ae4a5382f32e4307f43d11
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-07-26 15:40:11 +0300

    Frontend: Adding logo to admin navbar

--- medically-admin/src/components/NavBar.js
+++ medically-admin/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import image from '../assets/adminlogo.png';
 
 const Navbar = () => {
     let navigate = useNavigate();
@@ -12,11 +12,12 @@ const Navbar = () => {
     }
     return (
         <div className="navbar">
-            <div className="navbar-title">Admin Panel</div>
+            {/* <div className="navbar-title">Admin Panel</div> */}
+            <img src={image} className="navbar-logo" />
             <ul className="navbar-menu">
-                <li className="navbar-li" onClick={() => {navigate("/home", {replace: true})}}><div>Home</div></li>
-                <li className="navbar-li" onClick={() => {navigate("/requests", {replace: true})}}><div>Requests</div></li>
-                <li className="navbar-li" onClick={() => {navigate("/add-domain", {replace: true})}}><div>Add Domain</div></li>
+                <li className="navbar-li" onClick={() => { navigate("/home", { replace: true }) }}><div>Home</div></li>
+                <li className="navbar-li" onClick={() => { navigate("/requests", { replace: true }) }}><div>Requests</div></li>
+                <li className="navbar-li" onClick={() => { navigate("/add-domain", { replace: true }) }}><div>Add Domain</div></li>
                 <li className="navbar-li" onClick={() => { logoutFunction() }}><div>Logout</div></li>
             </ul>
         </div>
